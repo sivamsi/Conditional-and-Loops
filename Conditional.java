3af6e9b615bb480ad54098306235bb4eef78aeea
Vamsi Sisti <sistivamsi07@gmail.com>
2024-06-28 19:53:18 +0530

    feat : Checking postive or negative number using if-else

--- Conditional.java
+++ Conditional.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Conditional {
     public static void main(String[] args) {
         Scanner Sc=new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Enter your number");
         int num=Sc.nextInt();
         if(num>=0){
             System.out.println("Number is Positive : " + num);
